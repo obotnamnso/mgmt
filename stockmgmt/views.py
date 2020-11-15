b6dab6e5e1683e0de66903c596121ce78e17e86c
obotnamnso <benniiobot@gmail.com>
2020-11-15 22:30:55 +0100

    Rendering List from the database

--- stockmgmt/views.py
+++ stockmgmt/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Stock
 
 # Create your views here.
+def home(request):
+    title = "Home"
+    context = {
+        "title": title
+    }
+    return render(request, "stockmgmt/home.html", context)
+
+def list_item(request):
+    title = "List Item"
+    queryset = Stock.objects.all()
+    context = {
+        "title": title,
+        "queryset": queryset
+    }
+    return render(request, "stockmgmt/list_item.html", context)
