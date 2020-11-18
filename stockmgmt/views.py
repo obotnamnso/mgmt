f53d12d7d4c8401a04a8552fde38d048f7dd073e
obotnamnso <benniiobot@gmail.com>
2020-11-18 07:38:07 +0100

    Adding Static File CSS

--- stockmgmt/views.py
+++ stockmgmt/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Stock
 from .forms import StockCreateForm
 
@@ -22,6 +22,7 @@ def add_items(request):
     form = StockCreateForm(request.POST or None)
     if form.is_valid():
         form.save()
+        return redirect('/list_items')
     context = {
         "form": form,
         "title": "Add Item"
