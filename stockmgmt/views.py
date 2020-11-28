cedccafe87f279cd70cffc34d3ba5f64e255a300
obotnamnso <benniiobot@gmail.com>
2020-11-28 09:47:25 +0100

    How to Delete Data

--- stockmgmt/views.py
+++ stockmgmt/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Stock
-from .forms import StockCreateForm
+from .forms import *
 
 # Create your views here.
 def home(request):
@@ -12,11 +12,22 @@ def home(request):
 
 def list_items(request):
     header = "LIST OF PRODUCTS"
+    form = StockSearchForm(request.POST or None)  #inputing the form search.
     queryset = Stock.objects.all()
     context = {
         "header": header,
-        "queryset": queryset
+        "queryset": queryset,
+        "form": form,
     }
+    if request.method == 'POST':
+        queryset = Stock.objects.filter(category__icontains=form['category'].value(),
+                                        item_name__icontains=form['item_name'].value()
+                                        )
+        context = {
+                "form": form,
+                "header": header,
+                "queryset": queryset,
+            }
     return render(request, "stockmgmt/list_items.html", context)
 def add_items(request):
     form = StockCreateForm(request.POST or None)
@@ -28,3 +39,22 @@ def add_items(request):
         "title": "Add Item"
     }
     return render(request, "stockmgmt/add_items.html", context)
+def update_items(request, pk):
+    queryset = Stock.objects.get(id=pk)
+    form = StockUpdateForm(instance=queryset)
+    if request.method == "POST":
+        form = StockUpdateForm(request.POST, instance=queryset)
+        if form.is_valid():
+            form.save()
+            return redirect('/list_items')
+    context = {
+        "form": form
+    }
+    return render(request, 'stockmgmt/add_items.html', context)
+
+def delete_items(request, pk):
+	queryset = Stock.objects.get(id=pk)
+	if request.method == 'POST':
+		queryset.delete()
+		return redirect('/list_items')
+	return render(request, 'stockmgmt/delete_items.html')
