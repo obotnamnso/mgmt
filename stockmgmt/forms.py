ad4ea7fb75a25c0288fb329b48939e84716dd17c
obotnamnso <benniiobot@gmail.com>
2020-11-29 00:43:39 +0100

    updating-stock-level-issuing-and-receiving-stock

--- stockmgmt/forms.py
+++ stockmgmt/forms.py
@@ -29,3 +29,12 @@ class StockUpdateForm(forms.ModelForm):
     class Meta:
         model = Stock
         fields = ['category', 'item_name', 'quantity']
+class IssueForm(forms.ModelForm):
+	class Meta:
+		model = Stock
+		fields = ['issue_quantity', 'issue_to']
+
+class ReceiveForm(forms.ModelForm):
+	class Meta:
+		model = Stock
+		fields = ['receive_quantity']
--- stockmgmt/views.py
+++ stockmgmt/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
 import csv
 from .models import *
 from .forms import *
@@ -45,6 +46,7 @@ def add_items(request):
     form = StockCreateForm(request.POST or None)
     if form.is_valid():
         form.save()
+        messages.success(request, 'Item added Succssfully')
         return redirect('/list_items')
     context = {
         "form": form,
@@ -58,6 +60,7 @@ def update_items(request, pk):
         form = StockUpdateForm(request.POST, instance=queryset)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Succssfully Updated')
             return redirect('/list_items')
     context = {
         "form": form
@@ -65,8 +68,49 @@ def update_items(request, pk):
     return render(request, 'stockmgmt/add_items.html', context)
 
 def delete_items(request, pk):
+    queryset = Stock.objects.get(id=pk)
+    if request.method == 'POST':
+        queryset.delete()
+        messages.success(request,'Succssfully Updated')
+        return redirect('/list_items')
+    return render(request, 'stockmgmt/delete_items.html')
+
+def stock_detail(request, pk):
 	queryset = Stock.objects.get(id=pk)
-	if request.method == 'POST':
-		queryset.delete()
-		return redirect('/list_items')
-	return render(request, 'stockmgmt/delete_items.html')
+	context = {
+		"queryset": queryset,
+	}
+	return render(request, "stockmgmt/stock_detail.html", context)
+def issue_items(request, pk):
+    queryset = Stock.objects.get(id=pk)
+    form = IssueForm(request.POST or None, instance=queryset)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.quantity -= instance.issue_quantity
+        messages.success(request, "Issued SUCCESSFULLY." + str(instance.quantity) + " " + str(instance.item_name) + "s now left in store")
+        instance.save()
+        return redirect('/stock_detail/'+str(instance.id))
+    context = {
+        "title": 'Issue ' + str(queryset.item_name),
+        "form": form,
+        "username": 'Issue By: ' + str(request.user),
+    }
+    return render(request, "stockmgmt/add_items.html", context)
+
+
+def receive_items(request, pk):
+    queryset = Stock.objects.get(id=pk)
+    form = ReceiveForm(request.POST or None, instance=queryset)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.quantity += instance.receive_quantity
+        instance.save()
+        messages.success(request, "Received SUCCESSFULLY." + str(instance.quantity) + " " + str(instance.item_name)+"s now in store")
+        return redirect('/stock_detail/'+str(instance.id))
+    context = {
+        "title": 'Receive ' + str(queryset.item_name),
+        "instance": queryset,
+        "form": form,
+        "username": 'Receive By: ' + str(request.user),
+        }
+    return render(request, "stockmgmt/add_items.html", context)
